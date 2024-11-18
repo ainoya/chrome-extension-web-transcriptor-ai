c2e2b022ca2f200413d413fecdc4dbc4a63b64b0
Naoki Ainoya <ainonic@gmail.com>
2024-11-18 22:56:16 +0900

    use whisper-large-v3-turbo

--- src/jotai/modelStatusAtom.ts
+++ src/jotai/modelStatusAtom.ts
@@ -7,8 +7,8 @@ export type ModelStatus = "unknown" | "loading" | "ready" | "error";
 export const modelStatusAtom = atom<ModelStatus>("unknown");
 export const modelLoadingProgressAtom = atom<number>(0);
 export type transcriptionModelName =
-  | "onnx-community/whisper-base"
-  | "onnx-community/whisper-large-turbo-v3";
+	| "onnx-community/whisper-base"
+	| "onnx-community/whisper-large-turbo-v3";
 export const transcriptionModelAtom = atom<transcriptionModelName>(
-  "onnx-community/whisper-large-turbo-v3"
+	"onnx-community/whisper-large-turbo-v3",
 );
