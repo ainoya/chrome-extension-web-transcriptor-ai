2c48a25e3463d4a877e1908c2ded8fed6b686479
Naoki Ainoya <ainonic@gmail.com>
2024-11-18 20:42:20 +0900

    multilingual support

--- src/components/LanguageSelector.tsx
+++ src/components/LanguageSelector.tsx
@@ -1,5 +1,6 @@
+import type React from "react";
 import * as Select from "@radix-ui/react-select";
-import { CheckIcon, ChevronDownIcon } from "@radix-ui/react-icons";
+import { ChevronDownIcon, ChevronUpIcon } from "@radix-ui/react-icons";
 import { LANGUAGES, type TranscriptionLanguage } from "@/jotai/settingAtom";
 
 export const LanguageSelector: React.FC<{
@@ -10,38 +11,58 @@ export const LanguageSelector: React.FC<{
     setLanguage(value as TranscriptionLanguage);
   };
 
-  const names = Object.values(LANGUAGES).map(titleCase);
+  const names = Object.values(LANGUAGES);
 
   return (
-    <Select.Root value={language} onValueChange={handleLanguageChange}>
-      <Select.Trigger className="">
-        <Select.Value />
+    <Select.Root onValueChange={handleLanguageChange}>
+      <Select.Trigger className="inline-flex h-[35px] items-center justify-center gap-[5px] rounded px-[15px] text-[13px] leading-none outline-none hover:bg-mauve3 focus:shadow-[0_0_0_2px]">
+        <Select.Value
+          placeholder="Select a language…"
+          defaultValue={language}
+        />
         <Select.Icon>
           <ChevronDownIcon />
         </Select.Icon>
       </Select.Trigger>
-      <Select.Content>
-        <Select.ScrollUpButton />
-        <Select.Viewport>
-          {Object.keys(LANGUAGES).map((key, i) => (
-            <Select.Item
-              key={key}
-              value={key}
-              className="flex items-center p-2"
-            >
-              <Select.ItemText>{names[i]}</Select.ItemText>
-              <Select.ItemIndicator>
-                <CheckIcon />
-              </Select.ItemIndicator>
-            </Select.Item>
-          ))}
-        </Select.Viewport>
-        <Select.ScrollDownButton />
-      </Select.Content>
+      <Select.Portal>
+        <Select.Content 
+        className="overflow-hidden rounded-md bg-black shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)]">
+          <Select.ScrollUpButton className="flex h-[25px] cursor-default items-center justify-center">
+            <ChevronUpIcon />
+          </Select.ScrollUpButton>
+          <Select.Viewport className="p-[5px]">
+            <Select.Group>
+              <Select.Label className="px-[25px] text-xs leading-[25px]">
+                Languages
+              </Select.Label>
+              {names.map((name) => (
+                <SelectItem key={name} value={name}>
+                  {name}
+                </SelectItem>
+              ))}
+            </Select.Group>
+          </Select.Viewport>
+          <Select.ScrollDownButton className="flex h-[25px] cursor-default items-center justify-center">
+            <ChevronDownIcon />
+          </Select.ScrollDownButton>
+        </Select.Content>
+      </Select.Portal>
     </Select.Root>
   );
 };
 
-function titleCase(str: string) {
-  return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+interface SelectItemProps {
+  children: React.ReactNode;
+  value: string;
 }
+
+const SelectItem: React.FC<SelectItemProps> = ({ children, ...props }) => {
+  return (
+    <Select.Item
+      {...props}
+      className="relative flex h-[25px] select-none items-center px-[25px] text-[13px] leading-none data-[highlighted]:bg-violet9 data-[highlighted]:text-violet1"
+    >
+      <Select.ItemText>{children}</Select.ItemText>
+    </Select.Item>
+  );
+};
